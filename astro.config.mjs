c99ec2b538703b1e2f4c4c8caf2e25ad2cdcfc6a
Attituding <84680760+Attituding@users.noreply.github.com>
2022-08-30 23:23:33 -0700

    feat: improve accessibility feat: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,20 @@
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind({
-        config: {
-            applyBaseStyles: false,
-        },
-    })],
+    experimental: {
+        integrations: true,
+    },
+    integrations: [
+        sitemap(),
+        tailwind({
+            config: {
+                applyBaseStyles: false,
+            },
+        }),
+    ],
     site: 'https://shortsdeflector.attituding.live',
 });
